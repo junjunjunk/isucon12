c7af738c6e674803e63034cd88f76dd71a00e002
mizuno-hiroaki <mizuno-hiroaki@dmm.com>
2022-07-23 11:20:12 +0900

    3760 implement bulk insert

--- go/isuports.go
+++ go/isuports.go
@@ -1122,18 +1122,12 @@ func competitionScoreHandler(c echo.Context) error {
 	); err != nil {
 		return fmt.Errorf("error Delete player_score: tenantID=%d, competitionID=%s, %w", v.tenantID, competitionID, err)
 	}
-	for _, ps := range playerScoreRows {
-		if _, err := tenantDB.NamedExecContext(
-			ctx,
-			"INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)",
-			ps,
-		); err != nil {
-			return fmt.Errorf(
-				"error Insert player_score: id=%s, tenant_id=%d, playerID=%s, competitionID=%s, score=%d, rowNum=%d, createdAt=%d, updatedAt=%d, %w",
-				ps.ID, ps.TenantID, ps.PlayerID, ps.CompetitionID, ps.Score, ps.RowNum, ps.CreatedAt, ps.UpdatedAt, err,
-			)
 
-		}
+	_, err = tenantDB.NamedExec(`INSERT INTO player_score (id, tenant_id, player_id, competition_id, score, row_num, created_at, updated_at) VALUES (:id, :tenant_id, :player_id, :competition_id, :score, :row_num, :created_at, :updated_at)`, playerScoreRows)
+	if err != nil {
+		return fmt.Errorf(
+			"error Insert player_scores: %w", err,
+		)
 	}
 
 	return c.JSON(http.StatusOK, SuccessResult{
